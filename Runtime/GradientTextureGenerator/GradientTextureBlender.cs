5e93a5c239abdacd5075e794c916fbead2bbe84f
kejyunwu <kejyunwu@gmail.com>
2021-02-02 19:56:09 +0800

    Rename the parameters and function.

--- Runtime/GradientTextureGenerator/GradientTextureBlender.cs
+++ Runtime/GradientTextureGenerator/GradientTextureBlender.cs
@@ -7,6 +7,9 @@ namespace UltraCombos.VFXToolBox
 {
     public class GradientTextureBlender : GTBase
     {
+        /// <summary>
+        /// 
+        /// </summary>
         [SerializeField, LabelText("Select Index")] int m_index = 0;
         public int Index { get { return m_index; } set { m_index = value; } }
 
@@ -19,17 +22,29 @@ namespace UltraCombos.VFXToolBox
         [SerializeField] RenderTexture m_outputRT;
         public RenderTexture OutputRT { get { return m_outputRT; } }
 
+        /// <summary>
+        /// 
+        /// </summary>
         [SerializeField, HideInInspector] Shader m_shader;
-        Texture2D[] m_texture = new Texture2D[100]; //100 Magic Number ㄏ
+        Texture2D[] m_textures = new Texture2D[100]; //100 Magic Number ㄏ
+        Texture2DArray m_textureArray;
         Material m_mat;
 
+        /// <summary>
+        /// 
+        /// </summary>
         public int ClampIndex { get => Mathf.Clamp(m_index, 0, m_gradient.Count-1); }
+        public Texture2D GetSubGradientTextureByIndex(int _index) { return m_textures[Mathf.Clamp(_index, 0, m_gradient.Count - 1)]; }
+        public Texture2DArray GetAllGradientTextureWith2DArray() { UpdateTextureArray(ref m_textureArray, m_textures, m_gradient.Count); return m_textureArray; }
 
+        /// <summary>
+        /// 
+        /// </summary>
         void Reset()
         {
             for (var i = 0; i < m_gradient.Count; i++)
             {
-                UpdateTexture(ref m_texture[i], m_gradient[i]);
+                UpdateTexture(ref m_textures[i], m_gradient[i]);
             }
         }
 
@@ -37,16 +52,21 @@ namespace UltraCombos.VFXToolBox
         {
             m_mat = new Material(m_shader);
             Reset();
-            Graphics.Blit(m_texture[ClampIndex], m_outputRT);
+            Graphics.Blit(m_textures[ClampIndex], m_outputRT);
         }
 
         void Update()
         {
+            for (var i = 0; i < m_gradient.Count; i++)
+            {
+                UpdateTexture(ref m_textures[i], m_gradient[i]);
+            }
+
             if (m_outputRT != null && m_mat != null && m_gradient.Count != 0)
             {
                 m_mat.SetFloat("m_tweenSpeed", m_tweenSpeed);
                 m_mat.SetFloat("m_deltaTime", Time.deltaTime);
-                m_mat.SetTexture("m_gradient", m_texture[ClampIndex]);
+                m_mat.SetTexture("m_gradient", m_textures[ClampIndex]);
 
                 RenderTexture _temp = RenderTexture.GetTemporary(m_outputRT.descriptor);
                 Graphics.Blit(m_outputRT, _temp);
--- Runtime/GradientTextureGenerator/Script/GTBase.cs
+++ Runtime/GradientTextureGenerator/Script/GTBase.cs
@@ -10,34 +10,66 @@ namespace UltraCombos.VFXToolBox
         protected int m_resolution = 128;
         public int Resolution { get { return m_resolution; } set { m_resolution = value; } }
 
-        protected Texture2D CreateTex()
+        protected Texture2D CreateTexture()
         {
-            Texture2D _tex = new Texture2D(m_resolution, 1, TextureFormat.RGBAFloat, false);
+            Texture2D _tex = new Texture2D(m_resolution, 1, TextureFormat.RGBAFloat, false, false);
             _tex.Apply();
             return _tex;
         }
 
-        protected void UpdateTexture(ref Texture2D _tex, Gradient _gradient)
+        protected void UpdateTexture(ref Texture2D _texture, Gradient _gradient)
         {
-            if (_tex == null)
-                _tex = CreateTex();
+            if (_texture == null)
+                _texture = CreateTexture();
 
-            if (_tex.width != m_resolution)
+            if (_texture.width != m_resolution)
             {
                 if (Application.isPlaying)
-                    Destroy(_tex);
+                    Destroy(_texture);
                 else
-                    DestroyImmediate(_tex);
-                _tex = CreateTex();
+                    DestroyImmediate(_texture);
+                _texture = CreateTexture();
             }
 
             try
             {
                 for (int i = 0; i < m_resolution; i++)
                 {
-                    _tex.SetPixel(i, 0, _gradient.Evaluate(i / (float)m_resolution));
+                    _texture.SetPixel(i, 0, _gradient.Evaluate(i / (float)m_resolution));
                 }
-                _tex.Apply();
+                _texture.Apply();
+            }
+            catch { }
+        }
+
+        protected Texture2DArray CreateTextureArray(int _depth)
+        {
+            Texture2DArray _tex = new Texture2DArray(m_resolution, 1, _depth, TextureFormat.RGBAFloat, false);
+            _tex.Apply();
+            return _tex;
+        }
+
+        protected void UpdateTextureArray(ref Texture2DArray _textureArray, Texture2D[] _textures, int _count)
+        {
+            if (_textureArray == null)
+                _textureArray = CreateTextureArray(_count);
+
+            if (_textureArray.width != m_resolution)
+            {
+                if (Application.isPlaying)
+                    Destroy(_textureArray);
+                else
+                    DestroyImmediate(_textureArray);
+                _textureArray = CreateTextureArray(_count);
+            }
+
+            try
+            {
+                for (int i = 0; i < _count; i++)
+                {
+                    _textureArray.SetPixels(_textures[i].GetPixels(0), i, 0);
+                }
+                _textureArray.Apply();
             }
             catch { }
         }
