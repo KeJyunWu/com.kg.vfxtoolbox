31500c1ae7b7705e4a88be609edae66d0df051a2
kejyunwu <kejyunwu@gmail.com>
2021-02-16 20:11:26 +0800

    PointCacher - Velocity Data - Normal Data - Data Type { Buffer / RT / Array }
    GradientTexture
    - Added Material Apply

---
+++ Runtime/TinyTool/SimpleBlit/SimpleBlit.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteAlways]
+public class SimpleBlit : MonoBehaviour
+{
+    [SerializeField] Material m_mat;
+    [SerializeField] RenderTexture m_rt;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(m_mat != null && m_rt != null)
+        {
+            Graphics.Blit(null, m_rt, m_mat);
+        }
+    }
+}
