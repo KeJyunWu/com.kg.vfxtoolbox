7b7bfa998fa27ee6576d86622df14836b358ee6e
kejyunwu <kejyunwu@gmail.com>
2021-08-01 04:34:49 +0800

    新增 Float to texture

---
+++ Runtime/TransformToTexture/FloatToTexture.cs
@@ -0,0 +1,101 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Sirenix.OdinInspector;
+
+namespace UltraCombos.VFXToolBox
+{
+    [ExecuteInEditMode]
+    public class FloatToTexture : MonoBehaviour
+    {
+        [LabelText("Fixed Buffer Count")]
+        public bool m_bFixedBufferCount = false;
+        [ShowIf("m_bFixedBufferCount"), LabelText("Count"), Indent]
+        public int m_fixedBufferCount = 20;
+
+        [Space]
+        public List<float> m_floats = new List<float>();
+        public RenderTexture Result { get { return m_result; } }
+
+        [SerializeField, HideInInspector] ComputeShader m_shader;
+        ComputeBuffer m_buffer;
+        [SerializeField, ReadOnly] RenderTexture m_result;
+
+        int m_prevCount = 0;
+        float[] m_tempArray;
+
+        public void ForceUpdate()
+        {
+            Update();
+        }
+
+        void Release()
+        {
+            m_buffer?.Release();
+            m_buffer?.Dispose();
+            m_buffer = null;
+
+            if (m_result != null)
+                m_result?.Release();
+        }
+
+        bool Check()
+        {
+            if (m_floats == null || m_floats.Count == 0)
+            {
+                Release();
+                return false;
+            }
+
+            if (m_floats.Count != m_prevCount || m_buffer == null || m_tempArray == null)
+            {
+                m_prevCount = m_floats.Count;
+                Release();
+                m_result = RenderTextureUtil.Allocate(m_floats.Count, 1, RenderTextureFormat.ARGBFloat, FilterMode.Point);
+                m_buffer = new ComputeBuffer(m_floats.Count, sizeof(float));
+                m_tempArray = new float[m_floats.Count];
+                return true;
+            }
+
+            return true;
+        }
+
+        bool StaticCheck()
+        {
+            if (m_buffer == null || m_buffer.count != m_fixedBufferCount || m_tempArray == null)
+            {
+                Release();
+                m_result = RenderTextureUtil.Allocate(m_fixedBufferCount, 1, RenderTextureFormat.ARGBFloat, FilterMode.Point);
+                m_buffer = new ComputeBuffer(m_fixedBufferCount, sizeof(float));
+                m_tempArray = new float[m_fixedBufferCount];
+                return true;
+            }
+
+            return true;
+        }
+
+        private void Update()
+        {
+            if (m_bFixedBufferCount ? StaticCheck() : Check())
+            {
+                for (int i = 0; i < m_tempArray.Length; i++)
+                {
+                    m_tempArray[i] = 0;
+                    if (m_floats.Count > i)
+                        m_tempArray[i] = m_floats[i];
+                }
+
+                int _kernel = m_shader.FindKernel("CopyFloatData");
+                m_buffer.SetData(m_tempArray);
+                m_shader.SetTexture(_kernel, "m_result", m_result);
+                m_shader.SetBuffer(_kernel, "m_floatBuffer", m_buffer);
+                m_shader.Dispatch(_kernel, m_tempArray.Length, 1, 1);
+            }
+        }
+
+        private void OnDestroy()
+        {
+            Release();
+        }
+    }
+}
---
+++ Runtime/TransformToTexture/TransformToTexture.cs
@@ -0,0 +1,133 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Sirenix.OdinInspector;
+
+namespace UltraCombos.VFXToolBox
+{
+    [ExecuteInEditMode]
+    public class TransformToTexture : MonoBehaviour
+    {
+        public enum Mode
+        {
+            Position,
+            Rotation,
+            Scale
+        }
+
+        public Mode m_mode = Mode.Position;
+        [LabelText("Fixed Buffer Count")]
+        public bool m_bFixedBufferCount = false;
+        [ShowIf("m_bFixedBufferCount"), LabelText("Count"), Indent]
+        public int m_fixedBufferCount = 20;
+
+        [Space]
+        public List<Transform> m_transforms = new List<Transform>();
+        public RenderTexture Result { get { return m_result; } }
+
+        [SerializeField, HideInInspector] ComputeShader m_shader;
+        ComputeBuffer m_buffer;
+        [SerializeField, ReadOnly] RenderTexture m_result;
+
+        Vector3[] m_tempArray;
+
+        int m_prevCount = 0;
+
+        public void ForceUpdate()
+        {
+            Update();
+        }
+
+        public void AddTransform(Transform _t)
+        {
+            m_transforms.Add(_t);
+        }
+
+        public void RemoveTransform(Transform _t)
+        {
+            m_transforms.Remove(_t);
+        }
+
+        void Release()
+        {
+            m_buffer?.Release();
+            m_buffer?.Dispose();
+            m_buffer = null;
+
+            if(m_result!=null)
+                m_result?.Release();
+        }
+
+        bool Check()
+        {
+            if (m_transforms == null || m_transforms.Count == 0)
+            {
+                Release();
+                return false;
+            }
+
+            if (m_transforms.Count != m_prevCount || m_buffer == null || m_tempArray == null)
+            {
+                m_prevCount = m_transforms.Count;
+                Release();
+                m_result = RenderTextureUtil.Allocate(m_transforms.Count, 1, RenderTextureFormat.ARGBFloat, FilterMode.Point);
+                m_buffer = new ComputeBuffer(m_transforms.Count, sizeof(float) * 3);
+                m_tempArray = new Vector3[m_transforms.Count];
+                return true;
+            }
+
+            return true;
+        }
+
+        bool StaticCheck()
+        {
+            if (m_buffer == null || m_tempArray == null || m_buffer.count != m_fixedBufferCount)
+            {
+                Release();
+                m_result = RenderTextureUtil.Allocate(m_fixedBufferCount, 1, RenderTextureFormat.ARGBFloat, FilterMode.Point);
+                m_buffer = new ComputeBuffer(m_fixedBufferCount, sizeof(float) * 3);
+                m_tempArray = new Vector3[m_fixedBufferCount];
+                return true;
+            }
+
+            return true;
+        }
+
+        private void Update()
+        {
+            if (m_bFixedBufferCount ? StaticCheck() : Check())
+            {
+                for (int i = 0; i < m_tempArray.Length; i++)
+                {
+                    m_tempArray[i] = Vector3.one * 100000;
+                    if (m_transforms.Count > i)
+                    {
+                        switch (m_mode)
+                        {
+                            case Mode.Position:
+                                m_tempArray[i] = m_transforms[i].position;
+                                break;
+                            case Mode.Rotation:
+                                m_tempArray[i] = m_transforms[i].eulerAngles;
+                                break;
+                            default:
+                                m_tempArray[i] = m_transforms[i].localScale;
+                                break;
+                        }
+                    }
+                }
+
+                int _kernel = m_shader.FindKernel("CopyFloat3Data");
+                m_buffer.SetData(m_tempArray);
+                m_shader.SetTexture(_kernel, "m_result", m_result);
+                m_shader.SetBuffer(_kernel, "m_float3Buffer", m_buffer);
+                m_shader.Dispatch(_kernel, m_tempArray.Length, 1, 1);
+            }
+        }
+
+        private void OnDestroy()
+        {
+            Release();
+        }
+    }
+}
