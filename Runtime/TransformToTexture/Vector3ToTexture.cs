c87cd85b14e2c543ea6fe5ca0980874b326e18f1
kejyunwu <kejyunwu@gmail.com>
2021-08-09 03:56:44 +0800

    Added Vector3ToTexture

--- Runtime/TransformToTexture/Vector3ToTexture.cs
+++ Runtime/TransformToTexture/Vector3ToTexture.cs
@@ -6,15 +6,15 @@ using Sirenix.OdinInspector;
 namespace UltraCombos.VFXToolBox
 {
     [ExecuteInEditMode]
-    public class FloatToTexture : MonoBehaviour
+    public class Vector3ToTexture : MonoBehaviour
     {
         [LabelText("Fixed Buffer Count")]
         public bool m_bFixedBufferCount = false;
         [ShowIf("m_bFixedBufferCount"), LabelText("Count"), Indent]
         public int m_fixedBufferCount = 20;
-        public float m_defaultValue = 0;
+
         [Space]
-        public List<float> m_floats = new List<float>();
+        public List<Vector3> m_vectors = new List<Vector3>();
         public RenderTexture Result { get { return m_result; } }
 
         [SerializeField, HideInInspector] ComputeShader m_shader;
@@ -22,7 +22,7 @@ namespace UltraCombos.VFXToolBox
         [SerializeField, ReadOnly] RenderTexture m_result;
 
         int m_prevCount = 0;
-        float[] m_tempArray;
+        Vector3[] m_tempArray;
 
         public void ForceUpdate()
         {
@@ -41,19 +41,19 @@ namespace UltraCombos.VFXToolBox
 
         bool Check()
         {
-            if (m_floats == null || m_floats.Count == 0)
+            if (m_vectors == null || m_vectors.Count == 0)
             {
                 Release();
                 return false;
             }
 
-            if (m_floats.Count != m_prevCount || m_buffer == null || m_tempArray == null)
+            if (m_vectors.Count != m_prevCount || m_buffer == null || m_tempArray == null)
             {
-                m_prevCount = m_floats.Count;
+                m_prevCount = m_vectors.Count;
                 Release();
-                m_result = RenderTextureUtil.Allocate(m_floats.Count, 1, RenderTextureFormat.ARGBFloat, FilterMode.Point);
-                m_buffer = new ComputeBuffer(m_floats.Count, sizeof(float));
-                m_tempArray = new float[m_floats.Count];
+                m_result = RenderTextureUtil.Allocate(m_vectors.Count, 1, RenderTextureFormat.ARGBFloat, FilterMode.Point);
+                m_buffer = new ComputeBuffer(m_vectors.Count, sizeof(float)*3);
+                m_tempArray = new Vector3[m_vectors.Count];
                 return true;
             }
 
@@ -66,8 +66,8 @@ namespace UltraCombos.VFXToolBox
             {
                 Release();
                 m_result = RenderTextureUtil.Allocate(m_fixedBufferCount, 1, RenderTextureFormat.ARGBFloat, FilterMode.Point);
-                m_buffer = new ComputeBuffer(m_fixedBufferCount, sizeof(float));
-                m_tempArray = new float[m_fixedBufferCount];
+                m_buffer = new ComputeBuffer(m_fixedBufferCount, sizeof(float)*3);
+                m_tempArray = new Vector3[m_fixedBufferCount];
                 return true;
             }
 
@@ -80,15 +80,15 @@ namespace UltraCombos.VFXToolBox
             {
                 for (int i = 0; i < m_tempArray.Length; i++)
                 {
-                    m_tempArray[i] = m_defaultValue;
-                    if (m_floats.Count > i)
-                        m_tempArray[i] = m_floats[i];
+                    m_tempArray[i] = Vector3.one*1000;
+                    if (m_vectors.Count > i)
+                        m_tempArray[i] = m_vectors[i];
                 }
 
-                int _kernel = m_shader.FindKernel("CopyFloatData");
+                int _kernel = m_shader.FindKernel("CopyFloat3Data");
                 m_buffer.SetData(m_tempArray);
                 m_shader.SetTexture(_kernel, "m_result", m_result);
-                m_shader.SetBuffer(_kernel, "m_floatBuffer", m_buffer);
+                m_shader.SetBuffer(_kernel, "m_float3Buffer", m_buffer);
                 m_shader.Dispatch(_kernel, m_tempArray.Length, 1, 1);
             }
         }
