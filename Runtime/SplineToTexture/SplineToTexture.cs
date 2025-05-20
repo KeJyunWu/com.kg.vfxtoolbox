cd47b6c4c898bbc94ebd95651b8b648c837eeebf
kejyunwu <kejyunwu@gmail.com>
2025-05-20 00:27:54 -0700

    Unity Spline to texture

---
+++ Runtime/SplineToTexture/SplineToTexture.cs
@@ -0,0 +1,62 @@
+using Sirenix.OdinInspector;
+using UnityEngine;
+using UnityEngine.Splines;
+
+[RequireComponent(typeof(SplineContainer))]
+public class SplineToTexture : MonoBehaviour
+{
+    [SerializeField, Range(1,256),HideInPlayMode]
+    int m_resolutionWidth = 128;
+
+    [SerializeField]
+    RenderTexture m_outpurRT;
+
+    SplineContainer m_SplineContainer;
+    Texture2D m_dataMap;
+
+    Texture2D TextureCreating()
+    {
+        Texture2D _tex = new Texture2D(m_resolutionWidth, 1, TextureFormat.RGBAFloat, false, false);
+        _tex.filterMode = FilterMode.Point;
+        _tex.Apply();
+        return _tex;
+    }
+
+    void TextureInjection(Spline _spline)
+    {
+        if (m_dataMap == null)
+            return;
+        try
+        {
+            float _inv = 1f / (m_dataMap.width - 1);
+            for (int i = 0; i < m_resolutionWidth; i++)
+            {
+                Vector3 _p = _spline.EvaluatePosition(i * _inv);
+                _p += transform.position;
+                //Debug.Log(i);
+                m_dataMap.SetPixel(i, 0, new Color(_p.x, _p.y, _p.z,1));
+            }
+            m_dataMap.Apply();
+            //Debug.Log("===============");
+        }
+        catch { }
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        m_SplineContainer = GetComponent<SplineContainer>();
+        m_dataMap = TextureCreating();
+        m_outpurRT.width = m_dataMap.width;
+        m_outpurRT.height = m_dataMap.height;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Spline _s = m_SplineContainer.Spline;
+        TextureInjection(m_SplineContainer.Spline);
+        if(m_outpurRT != null)
+            Graphics.Blit(m_dataMap, m_outpurRT);
+    }
+}
